8f6ae6cd231eb05f4fc14f84da0923f6f246c18a
Sergey Pchelincev <jalkoby91@gmail.com>
2013-06-03 01:04:46 +0300

    add test application to docs

---
+++ living_things/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LivingThings::Application.config.secret_token = '972be944bdf6e58c07ff31e10dcd43fbb6388ee10ebc5839f78cdb4b15ef1de1d8f053c5fda22419b316a500a213e7fbeae00d57db965ee920ac5f396f3932f0'
